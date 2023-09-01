4a8973079626a74392194771401760bad97f8dcf
sepulveda272 <sepulvedarojasjuandavid@gmail.com>
2023-09-01 13:20:28 -0500

    feat: :sparkles: continuacion 2:03

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,29 +1,29 @@
- /* COLORES */
-    //Resetear color
-    const reset = "\x1b[0m";
-    //Colores de texto
-    const rojo = "\x1b[31m"
-    const verde = "\x1b[32m"
-    //Backgroun de texto
-    const backgroundRojo = "\x1b[41m";
-    const backgroundVerde = "\x1b[42m";
-    //Diseños de letra
-    const bold = "\x1b[1m";
-    const italic = "\x1b[3m";
-    //ESPACIO
-    const espacio = "/* -------------------------------------------------------------------------------------------------------------------------- */"
+/* COLORES */
+//Resetear color
+const reset = '\x1b[0m';
+//Colores de texto
+const rojo = '\x1b[31m';
+const verde = '\x1b[32m';
+//Backgroun de texto
+const backgroundRojo = '\x1b[41m';
+const backgroundVerde = '\x1b[42m';
+//Diseños de letra
+const bold = '\x1b[1m';
+const italic = '\x1b[3m';
+//ESPACIO
+const espacio =
+  '/* -------------------------------------------------------------------------------------------------------------------------- */';
 
 const { log } = require('console');
 //Codigo para verificar si funciona la consola
 
 /* const hello = "Hello world!";
-console.log(hello); */  
+console.log(hello); */
 
 // importacion de modulos
 const fs = require('fs');
-const http = require('http')
+const http = require('http');
 const url = require('url');
-
 
 //lectura de archivos
 
@@ -109,33 +109,64 @@ console.log('Will read file work 3!');  */
 });
 console.log('Will read file work 4!'); */
 
-
 ////////////////////////////////////////////////////////////////////////////////
 //SERVER
 
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
 
-const data = fs.readFileSync("./starter/dev-data/data.json", "utf-8")
-const dataObj = JSON.parse(data)
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  }
 
-const server = http.createServer((req,res) => {
-    const pathName = req.url;
+  return output;
+};
 
-    if (pathName === '/' || pathName === "/overview"){
-        res.end("this is the overview")
-    } else if (pathName === "/product"){
-        res.end("this is the product")
-    } else if (pathName === "/api"){
-        res.writeHead(200, {'Content-type':'application/json'})
-        res.end(data)
-    } else {
-        res.writeHead(404, {
-            'Content-type':'text/html',
-            'my-own-header': 'hello-word'
-        });
-        res.end("<h1>la pagina no funshion</h1>")
-    }
+const tempOverview = fs.readFileSync('./starter/templates/template-overview.html', 'utf-8');
+const tempCard = fs.readFileSync('./starter/templates/template-card.html', 'utf-8');
+const tempProduct = fs.readFileSync('./starter/templates/template-product.html', 'utf-8');
+
+const data = fs.readFileSync('./starter/dev-data/data.json', 'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  //Overview page
+  if (pathName === '/' || pathName === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+    res.end(output);
+
+    //Product page
+  } else if (pathName === '/product') {
+    res.end('this is the product');
+
+    //API
+  } else if (pathName === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
+
+    //Not found
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-word',
+    });
+    res.end('<h1>la pagina no funshion</h1>');
+  }
 });
 
-server.listen(8000, "127.0.01", ()=>{
-    console.log("se escucha en el puerto 8000");
-})
+server.listen(8000, '127.0.01', () => {
+  console.log('se escucha en el puerto 8000');
+});
